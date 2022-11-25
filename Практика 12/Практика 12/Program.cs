11f67148b5e3547e37c01f324a06233fde4615f5
ACER <ACER@DESKTOP-O7A416V>
2022-11-25 19:20:17 +0300

    Добавьте файлы проекта.

---
+++ Практика 12/Практика 12/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            int summ = 0;
+            Console.WriteLine("Для завершения ввода нажмите 0");
+            do
+            {
+                Console.WriteLine("Введите число");
+                index = int.Parse(Console.ReadLine());
+                summ += index;
+            }
+            while (index != 0);
+            Console.WriteLine("Сумма всех элементов = {0}", summ);
+            Console.WriteLine("Для выхода нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
